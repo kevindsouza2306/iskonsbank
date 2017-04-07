46f20f673c23e9383ab6188ae6e1649fa5c37148
KEVIN D'SOUZA <kevindsouza2306@gmail.com>
2017-04-07 22:37:51 +0530

    Add files via upload

---
+++ onlinebanking/imports/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        
+        <noscript><meta http-equiv="refresh" content="0;url=no-js.php"></noscript>    
+        
+        
+        <meta charset="UTF-8">
+        <title>Online Banking System</title>
+        <link rel="stylesheet" href="newcss.css">
+    </head>
+    <body>
+        <div class="wrapper">
+            
+        <div class="header">
+            <img src="header.jpg" height="100%" width="100%"/>
+            </div>
+            <div class="navbar">
+                
+            <ul>
+            <li><a href="index.php">Home </a></li>
+           <!-- <li><a href="features.php">Features </a></li>-->
+           <?php error_reporting(0)?>
+            <li><a href="contact.php">Contact Us</a></li>
+
+            <li><a href="register.php">Register</a></li>
+            <li id="last"><a href="login.php">Login</a></li>
+     
+            </ul>
+            </div>
